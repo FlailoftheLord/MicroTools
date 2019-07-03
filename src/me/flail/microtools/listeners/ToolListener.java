9d39af3702685840d0c32431ac3bfd6a3fcada7d
FlailoftheLord <jeddyh15@gmail.com>
2019-07-02 21:26:25 -0400

    slight progress... yea

--- src/me/flail/microtools/listeners/ToolListener.java
+++ src/me/flail/microtools/listeners/ToolListener.java
@@ -1,8 +1,13 @@
 package me.flail.microtools.listeners;
 
+import org.bukkit.entity.Item;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.entity.EntityDropItemEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -10,6 +15,7 @@ import org.bukkit.inventory.ItemStack;
 
 import me.flail.microtools.mct.mctool.MicroTool;
 import me.flail.microtools.tools.Logger;
+import me.flail.microtools.tools.Message;
 import me.flail.microtools.user.User;
 
 public class ToolListener extends Logger implements Listener {
@@ -37,7 +43,39 @@ public class ToolListener extends Logger implements Listener {
 
 	@EventHandler
 	public void pickUp(EntityPickupItemEvent event) {
+		LivingEntity entity = event.getEntity();
+		if (!(entity instanceof Player)) {
+			return;
+		}
 
+		User user = new User(((Player) entity).getUniqueId());
+
+		ItemStack item = event.getItem().getItemStack();
+		boolean isTool = hasTag(item, "tool");
+
+		if (isTool) {
+			MicroTool tool = MicroTool.fromItem(item);
+
+			if (!tool.hasOwner()) {
+				tool = tool.setOwner(user);
+
+				tool.updatePlaceholders(tool.placeholders());
+				return;
+			}
+			if (!tool.owner().id().equals(user.id())) {
+				event.setCancelled(true);
+
+				new Message("CannotPickupTool").send(user, null);
+			}
+
+		}
+
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void dropItem(EntityDropItemEvent event) {
+		Item itemEntity = event.getItemDrop();
+		ItemStack item = itemEntity.getItemStack();
 
 	}
 
